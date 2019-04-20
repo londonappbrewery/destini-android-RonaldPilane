a36626b04bcacdebd95417e1b0c52acc7e093fff
RonaldPilane <pilanerb@gmail.com>
2019-04-20 13:46:00 +0200

    My Solution

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView storyTextView;
+    Button buttonTop;
+    Button buttonBottom;
+    int mStoryIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +22,43 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
-
+        storyTextView = findViewById(R.id.storyTextView);
+        buttonTop = findViewById(R.id.buttonTop);
+        buttonBottom = findViewById(R.id.buttonBottom);
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
+        buttonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storyTextView.setText(R.string.T3_Story);
+                buttonTop.setText(R.string.T3_Ans1);
+                buttonBottom.setText(R.string.T3_Ans2);
+                mStoryIndex++;
+                trackGame();
+            }
+        });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        buttonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storyTextView.setText(R.string.T2_Story);
+                buttonTop.setText(R.string.T2_Ans1);
+                buttonBottom.setText(R.string.T2_Ans2);
+                mStoryIndex++;
+                trackGame();
 
+            }
+        });
 
     }
+    public void trackGame()
+    {
+        if(mStoryIndex == 5)
+        {
+            buttonBottom.setVisibility(View.GONE);
+            buttonTop.setVisibility(View.GONE);
+        }
+    }
+
 }
